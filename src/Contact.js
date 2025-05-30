3e73828ab53f473c4c9c59032316133abf2e6c2f
Ashish-C13 <ashishc1305@gmail.com>
2025-05-30 23:52:57 +0530

    Commited-Succesfully

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <>
+            <section id='Contact' className='contact'>
+                <h1 className='con-name'>Contact</h1>
+                <h4>If You Want To Discuss With Me</h4>
+                <h3><span className='cata'>E-Mail - </span>ashishcbscmaths2021@gmail.com</h3>
+                <h3><span className='cata'>Phone - </span>+91 63695 81744</h3>
+            </section>
+        </>
+    )
+}
---
+++ src/Resume.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ResumeImg from 'C:/React app/portfolio/src/portfolio-assets/resume-190916.jpg'
+export default function Resumer() {
+    return (
+        <>
+            <section id='Resume' className='resume'>
+                <div className='resume-name'>
+                    <h1>See my Resumea <span className='resume-link'><a href='#'>HEAR</a></span></h1>
+                </div>
+                <div className='resume-img'>
+                    <img className='r-img' src={ResumeImg}></img>
+                </div>
+            </section>
+        </>
+    )
+}
